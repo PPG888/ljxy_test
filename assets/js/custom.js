8ddf796ea8a77c20b6033f8a2cf93d591effb5be
PPG <1479487857@qq.com>
2024-09-01 17:00:27 +0800

    9.1上传测试
    9.1灵境官网上传测试

---
+++ assets/js/custom.js
@@ -0,0 +1,112 @@
+(function ($) {
+	
+	"use strict";
+
+	// Page loading animation
+	$(window).on('load', function() {
+
+        $('#js-preloader').addClass('loaded');
+
+    });
+
+
+	$(window).scroll(function() {
+	  var scroll = $(window).scrollTop();
+	  var box = $('.header-text').height();
+	  var header = $('header').height();
+
+	  if (scroll >= box - header) {
+	    $("header").addClass("background-header");
+	    // 添加以下代码来调整logo大小
+	    $(".header-area .main-nav .logo img").css("width", "200px"); // 调整这个值以适应您的需求
+	  } else {
+	    $("header").removeClass("background-header");
+	    // 恢复原始logo大小
+	    $(".header-area .main-nav .logo img").css("width", "238px"); // 或者您原始的logo宽度
+	  }
+	})
+
+	var width = $(window).width();
+		$(window).resize(function() {
+		if (width > 767 && $(window).width() < 767) {
+			location.reload();
+		}
+		else if (width < 767 && $(window).width() > 767) {
+			location.reload();
+		}
+	})
+
+	const elem = document.querySelector('.trending-box');
+	const filtersElem = document.querySelector('.trending-filter');
+	if (elem) {
+		const rdn_events_list = new Isotope(elem, {
+			itemSelector: '.trending-items',
+			layoutMode: 'masonry'
+		});
+		if (filtersElem) {
+			filtersElem.addEventListener('click', function(event) {
+				if (!matchesSelector(event.target, 'a')) {
+					return;
+				}
+				const filterValue = event.target.getAttribute('data-filter');
+				rdn_events_list.arrange({
+					filter: filterValue
+				});
+				filtersElem.querySelector('.is_active').classList.remove('is_active');
+				event.target.classList.add('is_active');
+				event.preventDefault();
+			});
+		}
+	}
+
+
+	// Menu Dropdown Toggle
+	if($('.menu-trigger').length){
+		$(".menu-trigger").on('click', function() {	
+			$(this).toggleClass('active');
+			$('.header-area .nav').slideToggle(200);
+		});
+	}
+
+
+	// Menu elevator animation
+	$('.scroll-to-section a[href*=\\#]:not([href=\\#])').on('click', function() {
+		if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+			var target = $(this.hash);
+			target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+			if (target.length) {
+				var width = $(window).width();
+				if(width < 991) {
+					$('.menu-trigger').removeClass('active');
+					$('.header-area .nav').slideUp(200);	
+				}				
+				$('html,body').animate({
+					scrollTop: (target.offset().top) - 80
+				}, 700);
+				return false;
+			}
+		}
+	});
+
+
+	// Page loading animation
+	$(window).on('load', function() {
+		if($('.cover').length){
+			$('.cover').parallax({
+				imageSrc: $('.cover').data('image'),
+				zIndex: '1'
+			});
+		}
+
+		$("#preloader").animate({
+			'opacity': '0'
+		}, 600, function(){
+			setTimeout(function(){
+				$("#preloader").css("visibility", "hidden").fadeOut();
+			}, 300);
+		});
+	});
+    
+
+
+})(window.jQuery);
